77173c50e6602fc175054b352b3d0ee2279ce822
Emanoel Eike Ferreira da Silva <eike.em.2017@gmail.com>
2021-02-20 09:19:58 -0300

    index modificado

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 include "Sevicos/MensagemDeErro.php";
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="author" content="">
@@ -10,68 +11,69 @@ include "Sevicos/MensagemDeErro.php";
     <meta name="viewport" content="Width=device-width, initial-scale=1">
     <title>Formulario cadastro de clientes</title>
 </head>
+
 <body>
     <form action="script.php" method="post">
-    <?php
-       $MensagemErro = obterMensagemErro();
-        if(!empty($MensagemErro)){
+        <?php
+        $MensagemErro = obterMensagemErro();
+        if (!empty($MensagemErro)) {
             echo $MensagemErro;
         }
         $MensagemSucesso = obterMensagemSucesso();
-        if(!empty($MensagemSucesso)){
+        if (!empty($MensagemSucesso)) {
             echo $MensagemSucesso;
         }
-    ?>
-    <!-- Dados pessoas -->
+        ?>
+        <!-- Dados pessoas -->
         <fieldset>
-         <legend> Dados Pessoas </legend>
-         <table cellspacing="10">
-         <tr>
-             <td>
-              <label for="nome">Nome:</label> 
-             </td>
-             <td align="left">
-               <input type="text" name="nome" size="43" maxlength="40" id="nome">
-             </td>
-            
-         </tr>
-         <tr>
-             <td>
-             <label>Nascimento:</label>
-             </td>
-             <td aling="left">
-             <input type="date" name="nacimento" id="">
-             <!-- <input type="text" name="dia" size="2" maxlength="2" value="dd">
+            <legend> Dados Pessoas </legend>
+            <table cellspacing="10">
+                <tr>
+                    <td>
+                        <label for="nome">Nome:</label>
+                    </td>
+                    <td align="left">
+                        <input type="text" name="nome" size="43" maxlength="40" id="nome">
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                        <label>Nascimento:</label>
+                    </td>
+                    <td aling="left">
+                        <input type="date" name="nacimento" id="">
+                        <!-- <input type="text" name="dia" size="2" maxlength="2" value="dd">
              <input type="text" name="mes" size="2" maxlength="2" value="mm">
              <input type="text" name="ano" size="4" maxlength="4" value="aaaa"> -->
-             </td>
-             <td>
-             <label for="email">E-mail:</label>
-             </td>
-             <td aling="left">
-               <input type="email" name="email">
-             </td>
-         </tr>
-         <tr> 
-          <td>
-          <label for="rg">GR:</label>
-          </td>
-          <td align="left">
-          <input type="text" name="rg" size="13" maxlength="13">
-          </td>
-         </tr>
-         <tr>
-             <td>
-                 <label>CPF:</label>
-             </td>
-                <td align="lefit">
-                <input type="text" name="cpf" size="9" maxlength="14"> 
-             </td>
-         </tr>
-         </table>
-        </fieldset> <br/>
+                    </td>
+                    <td>
+                        <label for="email">E-mail:</label>
+                    </td>
+                    <td aling="left">
+                        <input type="email" name="email">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="rg">GR:</label>
+                    </td>
+                    <td align="left">
+                        <input type="text" name="rg" size="13" maxlength="13">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>CPF:</label>
+                    </td>
+                    <td align="lefit">
+                        <input type="text" name="cpf" size="9" maxlength="14">
+                    </td>
+                </tr>
+            </table>
+        </fieldset> <br />
         <!-- ENDEREÇO -->
-        <fieldset >
+        <fieldset>
             <legend>Endereço</legend>
             <table cellspacing="10">
                 <tr>
@@ -89,12 +91,12 @@ include "Sevicos/MensagemDeErro.php";
                     </td>
                 </tr>
                 <tr>
-                    <td> 
+                    <td>
                         <label for="bairro">Bairro:</label>
                     </td>
                     <td aling="left">
-                    <input type="text" name="bairro" id="">
-                    </td> 
+                        <input type="text" name="bairro" id="">
+                    </td>
                 </tr>
                 <tr>
                     <td>
@@ -113,11 +115,12 @@ include "Sevicos/MensagemDeErro.php";
                     </td>
                 </tr>
             </table>
-        </fieldset><br/>
-        <input type="submit" value="Enviar" >
+        </fieldset><br />
+        <input type="submit" value="Enviar">
         <input type="reset" value="Limpar">
 
-    
+
     </form>
 </body>
+
 </html>
